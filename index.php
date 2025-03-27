93d9c1e0a5dcf2c795b11202f22b73092d279891
Phisbol <stan.magallon@gmail.com>
2025-03-27 16:05:15 +0800

    Add initial HTML structure and CSS styles for the UMakLib website

---
+++ index.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>UMAKLib</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <nav>
+        <div class="nav-wrapper">
+            <div>
+                <img src="img/umaklogo.png" class="umaklogo">
+                <h1>University Of Makati</h1>
+            </div>
+            <div id="nav-buttons">
+                <a href="#">Home</a>
+                <a href="#">Resources</a>
+                <a href="#">Digital Library</a>
+                <a href="#">Services</a>
+                <a href="#">About Us</a>
+                <a href="#">News</a>
+                <a href="#">Help</a>
+                <a href="#">My Account</a>
+            </div>
+        </div>
+    </nav>
+    <div class="container" style="padding-top:120px;">
+        <div class="wrapper">
+            <div class="home-search-container">
+                <p>Looking for something?</p>
+                <form>
+                    <div class="search-wrapper">
+                        <input><input type="image" src="img/search-icon.png" alt="Submit" id="searchButt">
+                    </div>
+                    <div class="search-radio">
+                        <div><input type="radio"> Keyword</div>
+                        <div><input type="radio"> Title</div>
+                        <div><input type="radio"> Author</div>
+                    </div>
+                </form>
+            </div>
+        </div>
+        <div class="banner-wrapper">
+            <img src="img/banner_1.jpeg">
+        </div>
+    </div>
+    <div class="container-g" style="margin-top:10vh;height:100vh">
+        <div class="wrapper">
+            <div class="news-wrapper">
+                <div class="news-container">
+                    <h2>GET THE LATEST NEWS IN UMAK</h2>
+                    <h1>Visit UMak.edu.ph Newsroom</h1>
+                    <div class="news-div">
+                        <div class="news-l">
+                            <div class="l-container">
+                                <div class="news-banner"></div>
+                                <p style="margin-top: 1vh;">March 11, 2025</p>
+                                <p>Application for CCAPS’ Professorial Chair 2025 is Now Open</p>
+                                <p>Professorial chairs are awarded to individuals of high esteem…</p>
+                            </div>
+                        </div>
+                        <div class="news-r">
+                            <div class="r-container">
+                                <div class="news-list">
+                                    <div class="news-icon-img"></div>
+                                    <div class="news-desc-wrap">
+                                        <p>MARCH 10, 20205</p>
+                                        <p>Makati Learners’ Convocation 2025: A Night of Gratitude, Elegance, and Lasting Memories</p>
+                                    </div>
+                                </div>
+                                <div class="news-list">
+                                    <div class="news-icon-img"></div>
+                                    <div class="news-desc-wrap">
+                                        <p>MARCH 10, 20205</p>
+                                        <p>Makati Learners’ Convocation 2025: A Night of Gratitude, Elegance, and Lasting Memories</p>
+                                    </div>
+                                </div>
+                                <div class="news-list">
+                                    <div class="news-icon-img"></div>
+                                    <div class="news-desc-wrap">
+                                        <p>MARCH 10, 20205</p>
+                                        <p>Makati Learners’ Convocation 2025: A Night of Gratitude, Elegance, and Lasting Memories</p>
+                                    </div>
+                                </div>
+                                <div class="news-list">
+                                    <div class="news-icon-img"></div>
+                                    <div class="news-desc-wrap">
+                                        <p>MARCH 10, 20205</p>
+                                        <p>Makati Learners’ Convocation 2025: A Night of Gratitude, Elegance, and Lasting Memories</p>
+                                    </div>
+                                </div>
+                                <div class="news-list">
+                                    <div class="news-icon-img"></div>
+                                    <div class="news-desc-wrap">
+                                        <p>MARCH 10, 20205</p>
+                                        <p>Makati Learners’ Convocation 2025: A Night of Gratitude, Elegance, and Lasting Memories</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+</body>
+
+</html>
