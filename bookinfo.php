fd229c1bdc479f4071e2e61452da5d3ce29f4675
Phisbol <stan.magallon@gmail.com>
2025-03-28 21:15:33 +0800

    Enhance bookinfo page layout and styling; update database query for book details

--- bookinfo.php
+++ bookinfo.php
@@ -1,15 +1,16 @@
 <?php
 include 'database.php';
 
-if (isset($_GET['ID'])) {
-    $book_id = mysqli_real_escape_string($conn, $_GET['ID']);
-    $query = "SELECT * FROM alumni_data WHERE id ='$book_id'";
+if (isset($_GET['id'])) {
+    $book_id = mysqli_real_escape_string($conn, $_GET['id']);
+    $query = "SELECT * FROM bookinfo WHERE id ='$book_id'";
     $query_run = mysqli_query($conn, $query);
 
     if (mysqli_num_rows($query_run) > 0) {
         $book = mysqli_fetch_array($query_run);
     }
 }
+
 
 ?>
 
@@ -27,11 +28,59 @@ if (isset($_GET['ID'])) {
     <?php
     include 'navbar.php';
     ?>
-    <div class="container" style="padding-top:120px;">
+    <div class="container" style="padding-top:140px;">
+        <div class="book-info-wrapper">
+            <div class="cover-handler">
+                <div class="book-info-container">
+                    <div class="book-info-cover">
+                        <div class="book-cover" style="background-image: url(book_cover/<?= $book['book_cover'] ?>);"></div>
+                    </div>
+                </div>
+            </div>
+            <div class="book-info-titles">
+                <div class="book-info-head"><?= $book['book_name'] ?></div>
+                <div class="book-info-subhead">
+                    AUTHOR:
+                    <p> <?= $book['book_author'] ?></p>
+                </div>
+                <div class="book-info-subhead">
+                    RATING:
+                    <p> <?= $book['book_rating'] ?></p>
+                </div>
+                <div class="book-info-subhead">
+                    STATUS:
+                    <p> <?= $book['book_status'] ?></p>
+                </div>
+                <div class="book-info-subhead">
+                    CALL NO:
+                    <p> <?= $book['book_callno'] ?></p>
+                </div>
+            </div>
+        </div>
 
-        <?= $book['book_name'] ?>
-
-
+        <div class="summary-wrapper">
+            <div>
+                <div class="book-info-head">SUMMARY</div>
+                <div class="book-summary">
+                    <?= $book['book_summary'] ?>
+                </div>
+            </div>
+            <div class="book-details-wrap">
+                <div class="book-info-head">DETAILS</div>
+                <div class="book-info-subhead">
+                    GENRE:
+                    <p> <?= $book['book_genre'] ?></p>
+                </div>
+                <div class="book-info-subhead">
+                    ISBN:
+                    <p> <?= $book['book_ISBN'] ?></p>
+                </div>
+                <div class="book-info-subhead">
+                    QUANTITY:
+                    <p> <?= $book['book_quantity'] ?></p>
+                </div>
+            </div>
+        </div>
     </div>
     <?php
     include 'footer.php'
